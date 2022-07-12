59b6d066ad5d859dc814673c0d9f6ce9a6f840b9
Rob Patro <rob@newton>
2022-07-12 17:18:50 -0400

    almost working

--- build.rs
+++ build.rs
@@ -5,30 +5,44 @@ fn main() {
     let custom_cc = env::var("CC");
     let custom_cxx = env::var("CXX");
 
-    let mut cfg = Box::new(Config::new("piscem-cpp"));
+    let mut cfg_piscem_cpp = Box::new(Config::new("piscem-cpp"));
+    let mut cfg_cf = Box::new(Config::new("cuttlefish"));
+
+    (*cfg_cf).define("INSTANCE_COUNT", "32");
     match custom_cc {
         Ok(cc_var) => {
-            (*cfg).define("CMAKE_C_COMPILER", cc_var);
+            (*cfg_piscem_cpp).define("CMAKE_C_COMPILER", cc_var.clone());
+            (*cfg_cf).define("CMAKE_C_COMPILER", cc_var);
         }
         Err(_) => {}
     }
 
     match custom_cxx {
         Ok(cxx_var) => {
-            (*cfg).define("CMAKE_CXX_COMPILER", cxx_var);
+            (*cfg_piscem_cpp).define("CMAKE_CXX_COMPILER", cxx_var.clone());
+            (*cfg_cf).define("CMAKE_CXX_COMPILER", cxx_var);
         }
         Err(_) => {}
     }
 
-    let dst = (*cfg).build();
+    let dst_piscem_cpp = (*cfg_piscem_cpp).build();
+    let dst_cf = (*cfg_cf).build();
 
     println!(
         "cargo:rustc-link-search=native={}",
-        dst.join("lib").display()
+        dst_cf.join("lib").display()
     );
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst_piscem_cpp.join("lib").display()
+    );
+    
+
+    println!("cargo:rustc-link-lib=static=kmc_core");
     println!("cargo:rustc-link-lib=static=pesc_static");
     println!("cargo:rustc-link-lib=static=build_static");
     println!("cargo:rustc-link-lib=static=sshash_static");
     println!("cargo:rustc-link-lib=static=z");
+    println!("cargo:rustc-link-lib=static=bz2");
     println!("cargo:rustc-link-lib=dylib=stdc++");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
 
-use clap::{arg, command, Command}; 
 use anyhow::{bail, Result};
+use clap::{arg, command, value_parser, Command};
 
 #[link(name = "pesc_static", kind = "static")]
 extern "C" {
     pub fn run_pesc(args: c_int, argsv: *const *const c_char) -> c_int;
+}
+
+
+#[link(name = "build_static", kind = "static")]
+extern "C" {
     pub fn run_build(args: c_int, argsv: *const *const c_char) -> c_int;
+}
+
+#[link(name = "cfcore_static", kind = "static", modifiers = "+whole-archive")]
+extern "C" {
+    pub fn cf_build(args: c_int, argsv: *const *const c_char) -> c_int;
 }
 
 fn print_help() {
@@ -15,7 +25,6 @@ fn print_help() {
     eprintln!("valid commands are {{build, map}}, for more information ");
     eprintln!("try build -h or map -h.");
 }
-
 
 /*
 Options:
@@ -31,70 +40,166 @@ Options:
  */
 
 fn main() -> Result<(), anyhow::Error> {
-
     let matches = command!()
         .propagate_version(true)
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
             Command::new("build")
-            .about("build the piscem index")
-            .args(&[
-                  arg!(-r --ref <REF> "reference FASTA location"),
-                  arg!(-k --klen <K> "length of k-mer to use"),
-                  arg!(-t --threads <THREADS> "number of threads to use"),
-                  arg!(-o --output <OUTPUT> "output file stem")
-            ])
+                .about("build the piscem index")
+                .args(&[
+                    arg!(-r --reference <REFERENCE> "reference FASTA location"),
+                    arg!(-k --klen <K> "length of k-mer to use").value_parser(value_parser!(usize)),
+                    arg!(-m --mlen <M> "length of the minimizer use")
+                        .value_parser(value_parser!(usize)),
+                    arg!(-t --threads <THREADS> "number of threads to use")
+                        .value_parser(value_parser!(usize)),
+                    arg!(-o --output <OUTPUT> "output file stem"),
+                ]),
         )
         .subcommand(
-            Command::new("map")
-            .about("map reads")
-            .args(&[
-                  arg!(-i --index <INDEX> "input index prefix"),
-                  arg!(-'1' --read1 <READ1> "path to list of read 1 files").require_value_delimiter(true), 
-                  arg!(-'2' --read2 <READ2> "path to list of read 2 files").require_value_delimiter(true), 
-                  arg!(-o --output <OUTPUT> "path to output directory"),
-                  arg!(-g --geometry <GEO> "geometry of barcode, umi and read"),
-                  arg!(-t --threads <THREADS> "an interger specifying the number of threads to use")
-            ])
-        ).get_matches();
+            Command::new("map").about("map reads").args(&[
+                arg!(-i --index <INDEX> "input index prefix"),
+                arg!(-'1' --read1 <READ1> "path to list of read 1 files")
+                    .require_value_delimiter(true),
+                arg!(-'2' --read2 <READ2> "path to list of read 2 files")
+                    .require_value_delimiter(true),
+                arg!(-o --output <OUTPUT> "path to output directory"),
+                arg!(-g --geometry <GEO> "geometry of barcode, umi and read"),
+                arg!(-t --threads <THREADS> "an interger specifying the number of threads to use")
+                    .value_parser(value_parser!(u32)),
+            ]),
+        )
+        .get_matches();
 
-    if std::env::args().len() < 2 {
-        print_help();
-        bail!("program exited abnormally.");
-    } else {
-        // treat the 1st argument as the "command"
-        let cmd = std::env::args().nth(1).expect("no command given");
-        // from
-        // https://stackoverflow.com/questions/69437925/problem-with-calling-c-function-that-receive-command-line-arguments-from-rust
-        let mut args = std::env::args()
-            .map(|arg| CString::new(arg).unwrap())
-            .collect::<Vec<CString>>();
-        _ = args.remove(1);
-        let arg_ptrs: Vec<*const c_char> = args.iter().map(|s| s.as_ptr()).collect();
-        let args_len: c_int = args.len() as c_int;
+    match matches.subcommand() {
+        Some(("build", sub_matches)) => {
+            let mut args: Vec<CString> = vec![];
+            let k: usize = *sub_matches.get_one("klen").expect("K should be an integer");
+            let m: usize = *sub_matches.get_one("mlen").expect("M should be an integer");
+            let r: String = sub_matches
+                .get_one::<String>("reference")
+                .expect("REFERENCE missing")
+                .to_string();
+            let t: usize = *sub_matches
+                .get_one("threads")
+                .expect("THREADS should be an integer");
+            let o: String = sub_matches
+                .get_one::<String>("output")
+                .expect("OUTPUT missing")
+                .to_string();
 
-        match cmd.as_str() {
-            "map" => {
-                unsafe { run_pesc(args_len, arg_ptrs.as_ptr()) };
+            assert!(m < k, "minimizer length ({}) >= k-mer len ({})", m, k);
+
+            let cf_out = o.clone() + "_cfish";
+            let mut build_ret = 0;
+            /*
+            args.push(CString::new("cdbg_builder").unwrap());
+            args.push(CString::new("--seq").unwrap());
+            args.push(CString::new(r.as_str()).unwrap());
+            args.push(CString::new("-k").unwrap());
+            args.push(CString::new(k.to_string()).unwrap());
+            args.push(CString::new("-o").unwrap());
+            args.push(CString::new(cf_out.as_str()).unwrap());
+            args.push(CString::new("-t").unwrap());
+            args.push(CString::new(t.to_string()).unwrap());
+            // format
+            args.push(CString::new("-f").unwrap());
+            args.push(CString::new("3").unwrap());
+
+            {
+                let arg_ptrs: Vec<*const c_char> = args.iter().map(|s| s.as_ptr()).collect();
+                let args_len: c_int = args.len() as c_int;
+                build_ret = unsafe { cf_build(args_len, arg_ptrs.as_ptr()) };
             }
-            "build" => {
-                unsafe { run_build(args_len, arg_ptrs.as_ptr()) };
-            }
-            "--version" => {
-                eprintln!("piscem version 0.0.1");
-            }
-            "-h" | "--help" => {
-                print_help();
-            }
-            c => {
-                eprintln!(
-                    "{} is not a valid command; valid commands are {{build, map}}.",
-                    c
+
+            if build_ret != 0 {
+                bail!(
+                    "cDBG constructor returned exit code {}; failure.",
+                    build_ret
                 );
-                bail!("program exited abnormally.");
+            }
+
+            args.clear();
+            */
+            args.push(CString::new("ref_index_builder").unwrap());
+            args.push(CString::new(cf_out.as_str()).unwrap());
+            args.push(CString::new(k.to_string()).unwrap());
+            args.push(CString::new(m.to_string()).unwrap()); // minimizer length
+            args.push(CString::new("--canonical-parsing").unwrap());
+            args.push(CString::new("-o").unwrap());
+            args.push(CString::new(o.as_str()).unwrap());
+            {
+                println!("{:?}", args);
+                let arg_ptrs: Vec<*const c_char> = args.iter().map(|s| s.as_ptr()).collect();
+                let args_len: c_int = args.len() as c_int;
+                build_ret = unsafe { run_build(args_len, arg_ptrs.as_ptr()) };
+            }
+
+            if build_ret != 0 {
+                bail!("indexer returned exit code {}; failure.", build_ret);
             }
         }
+        Some(("map", sub_matches)) => {
+            let mut args: Vec<CString> = vec![];
+            let r1: String = sub_matches
+                .get_many("read1")
+                .expect("read2 empty")
+                .cloned()
+                .collect::<Vec<String>>()
+                .join(",");
+            let r2: String = sub_matches
+                .get_many("read2")
+                .expect("read1 empty")
+                .cloned()
+                .collect::<Vec<String>>()
+                .join(",");
+            let i: String = sub_matches
+                .get_one::<String>("index")
+                .expect("INDEX missing")
+                .to_string();
+            let t: u32 = *sub_matches
+                .get_one("threads")
+                .expect("THREADS should be an integer");
+            let g: String = sub_matches
+                .get_one::<String>("geometry")
+                .expect("OUTPUT missing")
+                .to_string();
+            let o: String = sub_matches
+                .get_one::<String>("output")
+                .expect("OUTPUT missing")
+                .to_string();
+
+            args.push(CString::new("ref_mapper").unwrap());
+            args.push(CString::new("-i").unwrap());
+            args.push(CString::new(i.as_str()).unwrap());
+            args.push(CString::new("-g").unwrap());
+            args.push(CString::new(g.to_string()).unwrap());
+
+            args.push(CString::new("-1").unwrap());
+            args.push(CString::new(r1.as_str()).unwrap());
+
+            args.push(CString::new("-2").unwrap());
+            args.push(CString::new(r2.as_str()).unwrap());
+
+            args.push(CString::new("-t").unwrap());
+            args.push(CString::new(t.to_string()).unwrap());
+
+            args.push(CString::new("-o").unwrap());
+            args.push(CString::new(o.as_str()).unwrap());
+
+            let arg_ptrs: Vec<*const c_char> = args.iter().map(|s| s.as_ptr()).collect();
+            let args_len: c_int = args.len() as c_int;
+
+            unsafe { run_pesc(args_len, arg_ptrs.as_ptr()) };
+        }
+        Some((cmd, &_)) => {
+            bail!("Invalid command {}; program exited abnormally.", cmd);
+        }
+        None => {
+            bail!("missing command; program exited abnormally.");
+        }
     }
+
     Ok(())
 }
